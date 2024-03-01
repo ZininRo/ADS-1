297f571161834c06f27bbf8da8d7dba06c19f9bb
ZininRo <151348599+ZininRo@users.noreply.github.com>
2024-03-02 01:17:01 +0300

    Зинин Роман 23ИСТ-4-1. Задание 1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,20 +1,49 @@
 // Copyright 2022 NNTU-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <valarray>
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if((value < 2) || (value % 2 == 0)) {
+        return false;
+    }
+    for (uint64_t i = 3; i <= sqrt(value); i += 2) {
+        if(value % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int count = 1;
+    int number_now = 2;
+    for(int i = 3; count < n; i += 2) {
+        if (checkPrime(i)) {
+            count++;
+            number_now = i;
+        }
+    }
+    return number_now;
 }
 
+
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    value++;
+    while(!checkPrime(value)) {
+        value++;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    uint64_t number = 2;
+    uint64_t summ = 0;
+    while (number < hbound) {
+        if (checkPrime(number)) {
+            summ += number;
+        }
+        number++;
+    }
+    return summ;
 }
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include "alg.h"
 
+
 int main() {
     return 0;
 }
+
+
